48f4e62348aee1f1144347339e0e60281dd96360
nick.harte1 <nick.harte1@zone17-wb.cs.ucalgary.ca>
2020-02-21 12:38:46 -0700

    Started the main class

---
+++ src/gameofuofc/Main.java
@@ -0,0 +1,33 @@
+package gameofuofc;
+public class Main {
+
+		board boardObject = new board();
+		player playerObject = new player(0, null);
+		player player1Object = new player(1, null);
+		squares FinalSquare = new square();
+		FinalSquare = ("Final Square", false, 'e', 23);
+	
+		playerObject.setPlayerLocation(boardObject.getsquare(0));
+		
+		player1Object.setPlayerLocation(boardObject.getsquare(0));
+		
+		public void main() {
+			while((playerObject.getType() && player1Object.getType()) != FinalSquare){
+				
+				playerObject.spin();
+				
+				playerObject.moveLocation();
+				
+				player1Object.spin(); 
+				
+				player1Object.moveLocation();
+				
+				
+				
+			}
+		
+		
+		
+	}
+
+}
