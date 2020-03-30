3133a9b4c7a189352d13a414eb28451280f69977
Tin Le <tin.le@ucalgary.ca>
2020-03-29 23:29:58 -0600

    Added comments for javadoc in Board.java

--- gameofuofc-master/src/gameofuofc/Board.java
+++ gameofuofc-master/src/gameofuofc/Board.java
@@ -7,6 +7,11 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+/**
+ * The class Board creates and initializes a new board based on the information about squares stored in a csv file.
+ * In the program, the game's board is essentially an ArrayList of Squares. Each square is an object, that has different properties.
+ */
+
 public class Board {
 	ArrayList<Square> squares = new ArrayList<Square>();
 	public Board() {
@@ -25,6 +30,13 @@ public class Board {
 	    }
 	    // this code is copied from my hackathon app
 	}
+	
+	/**
+	 * The constructor creates a board using the information in the board csv file. It then adds all the squares in the csv into the squares ArrayList, which is essentially the game's board.
+	 * @param None
+	 * @return nothing
+	 */
+	
 	public Square getSquare(int index) {
 		return squares.get(index);
 	}
