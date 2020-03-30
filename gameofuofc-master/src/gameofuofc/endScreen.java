08775ed14bbf2468056524aeceaafab76d39eabc
roubertdili <60020388+roubertdili@users.noreply.github.com>
2020-03-29 18:34:56 -0600

    Update endScreen.java

--- gameofuofc-master/src/gameofuofc/endScreen.java
+++ gameofuofc-master/src/gameofuofc/endScreen.java
@@ -11,6 +11,11 @@ import javafx.scene.control.Button;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
+/**
+* The class endScreen contains methods that display the end of the game ends 
+* and give the user the option to reveal winner. 
+*/
+
 public class endScreen {
 
     @FXML
@@ -20,12 +25,21 @@ public class endScreen {
     private Button revealbtn;
     static String winner;
     Stage window = new Stage();
+	
+    /** The method showEnd(String awinner) launches the display that the game has ended. 
+    * It contains a button that would reveal the winner. It takes winner of type String, 
+    * throws IOException, and returns nothing.
+    */
+	
     void showEnd(String awinner) throws IOException {
     	winner = awinner;
 		Parent root = FXMLLoader.load(Main.class.getResource("WinScreen.fxml"));
 		window.setScene(new Scene(root));
 		window.showAndWait();
     }
+	
+    // This method handles the event at which the button has been clicked and reveals 
+    // the winner. 
     @FXML
     void reveal(ActionEvent event) {
     	textbox.setText(winner + " has won!");
